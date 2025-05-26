bb9bb10260536bd44c783581b6df05720ed07a98
ZizahSwiftie <azizahulnha@gmail.com>
2025-05-26 19:34:19 +0700

    feat: Implement Pasien-Dokter association logic and demo

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -37,3 +37,54 @@ public:
     void tambahPasien(pasien* pPasien);
     void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+    pDokter->tambahPasien(this);
+}
+
+void pasien::cetakDokter() {
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& d : daftar_dokter) {
+        cout << "- " << d->nama << "\n";
+    }
+    cout << endl;
+}
+
+void dokter::tambahPasien(pasien* pPasien) {
+    daftar_pasien.push_back(pPasien);
+}
+
+void dokter::cetakPasien() {
+    cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto& p : daftar_pasien) {
+        cout << "- " << p->nama << "\n";
+    }
+    cout << endl;
+}
+
+int main() {
+    dokter* varDokter1 = new dokter("Dr. Budi");
+    dokter* varDokter2 = new dokter("Dr. Tono");
+
+    pasien* varPasien1 = new pasien("Andi");
+    pasien* varPasien2 = new pasien("Lia");
+
+    varDokter1->tambahPasien(varPasien1);
+    varDokter1->tambahPasien(varPasien2);
+
+    varDokter2->tambahPasien(varPasien1);
+
+    varDokter1->cetakPasien();
+    varDokter2->cetakPasien();
+
+    varPasien1->cetakDokter();
+    varPasien2->cetakDokter();
+
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+
+    return 0;
+}
