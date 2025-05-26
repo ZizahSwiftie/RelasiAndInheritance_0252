61f2d11cd9ad2a302342a89d954988f33c874be2
ZizahSwiftie <azizahulnha@gmail.com>
2025-05-26 19:39:26 +0700

    kelas turunan

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -19,3 +19,32 @@ public:
         return a + b;
     }
 };
+
+// Kelas turunan
+class pelajar : public orang {
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) 
+        : orang(pNama), sekolah(pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+
+    ~pelajar() {
+        cout << "Pelajar dihapus\n" << endl;
+    }
+
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + 
+               " dari sekolah " + sekolah + "\n";
+    }
+};
+
+int main() {
+    pelajar siswa1("Andi Laksono", "SMAN 1 Bantul");
+
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
