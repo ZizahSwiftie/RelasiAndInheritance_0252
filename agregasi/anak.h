002dc92a3207e66b4bffa20aebf0897c43a21801
ZizahSwiftie <azizahulnha@gmail.com>
2025-05-26 18:29:07 +0700

    Add 'ibu' class with support for managing anak instances

--- agregasi/anak.h
+++ agregasi/anak.h
@@ -14,3 +14,21 @@ public:
         cout << "Anak \"" << nama << "\" tidak ada\n";
     }
 };
+#endif
+
+#ifndef IBU_H
+#define IBU_H
+
+class ibu
+{
+public:
+    string nama;
+    vector<anak> daftar_anak;
+
+    ibu(string pNama) : nama(pNama) 
+    {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
